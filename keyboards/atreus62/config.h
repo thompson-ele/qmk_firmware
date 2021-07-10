2bf21850e073375bf3d1bdc53e3d5458a4dd09e9
Ele Thompson <ele.thompson12@gmail.com>
2021-07-10 13:25:25 -0700

    [Keymap] Added VIA support to Atreus62

--- keyboards/atreus62/config.h
+++ keyboards/atreus62/config.h
@@ -22,7 +22,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* USB Device descriptor parameter */
 
-#define VENDOR_ID       0xFEED
+#define VENDOR_ID       0x1606
 #define PRODUCT_ID      0x6062
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    Profet
